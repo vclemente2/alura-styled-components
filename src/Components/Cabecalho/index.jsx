8c657d36b019c523d238d0f59d1667723b5b11d4
Vinicius Bastos <vclemente2@hotmail.com>
2023-05-29 18:02:28 -0300

    Implementação do styled-components nas seções cabecalho e container.

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,19 +1,46 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
+import { corPrimaria } from "../UI/variaveis"
+
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  color: ${({ primary }) => primary ? "white" : corPrimaria};
+  background: ${({ primary }) => primary ? corPrimaria : "white"};
+`;
+
+const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+    <StyledHeader>
+      <Logo className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnCabecalho href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnCabecalho>
+        <BtnCabecalho primary href="https://google.com">
           Sair
-        </a>
+        </BtnCabecalho>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
